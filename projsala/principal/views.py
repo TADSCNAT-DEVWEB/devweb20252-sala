abb697c3d0ebefd54cfb153b34facb55a5cbdcd2
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-10-27 15:49:04 -0300

    Remoção de código sem utilizada na view index da aplicação principal.

--- projsala/principal/views.py
+++ projsala/principal/views.py
@@ -3,12 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    """
-    View para a página inicial do projeto.
-    Demonstra o uso de herança de templates.
-    """
-    context = {
-        'titulo': 'Página Inicial',
-        'descricao': 'Sistema de demonstração de templates Django com Bulma CSS'
-    }
-    return render(request, 'principal/index.html', context)
+ 
+    return render(request, 'principal/index.html')
