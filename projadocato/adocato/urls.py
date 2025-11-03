2576361366b897789728f49ca005deab83bc09d3
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-11-03 15:26:03 -0300

    Remoção de referência a view não existente, para fins didáticos.

--- projadocato/adocato/urls.py
+++ projadocato/adocato/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('racas/', views.raca_list, name='raca_list'),
     path('gatos/', views.gato_list, name='gato_list'),
-    path('racas/<int:raca_id>/gatos/', views.gato_list_por_raca, name='gato_por_raca'),
+   #path('racas/<int:raca_id>/gatos/', views.gato_list_por_raca, name='gato_por_raca'),
 ]
