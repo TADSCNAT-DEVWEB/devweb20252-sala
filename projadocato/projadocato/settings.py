8372c902ee7c651a4237fac2a9842193181349ee
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-11-03 15:24:58 -0300

    Implementação dos templates para gatos.

--- projadocato/projadocato/settings.py
+++ projadocato/projadocato/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "adocato",
     "django_extensions",
 ]
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
