8e663cd31471e73a352f52f466e607487743db53
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-11-05 21:00:37 +0000

    Alterações no modelo.

--- projadocato/adocato/models.py
+++ projadocato/adocato/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Raca(models.Model):
     nome=models.CharField(max_length=100,unique=True)
 
+    def __str__(self):
+        return self.nome
+
 class Gato(models.Model):
     nome=models.CharField(max_length=100)
     sexo=models.CharField(max_length=1,choices=[('M','Macho'),('F','Femea')])
@@ -13,3 +16,12 @@ class Gato(models.Model):
     descricao=models.TextField(blank=True,null=True)
     disponivel=models.BooleanField(default=True)
     raca=models.ForeignKey(Raca,on_delete=models.CASCADE,related_name='gatos')
+
+    @property
+    def idade(self):
+        from datetime import date
+        hoje=date.today()
+        idade=hoje.year - self.data_nascimento.year
+        if (hoje.month,hoje.day) < (self.data_nascimento.month,self.data_nascimento.day):
+            idade -= 1
+        return idade
