089cb5e78a854887f70eb100878ad1aadf7f96fc
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-11-13 17:29:45 +0000

    Adição do atributo foto no modelo de gatos e configuração para servir conteúdo de arquivo de media.

--- projadocato/adocato/models.py
+++ projadocato/adocato/models.py
@@ -9,13 +9,14 @@ class Raca(models.Model):
         return self.nome
 
 class Gato(models.Model):
-    nome=models.CharField(max_length=100)
+    nome=models.CharField(max_length=100,verbose_name='Nome do Gato')
     sexo=models.CharField(max_length=1,choices=[('M','Macho'),('F','Femea')])
     cor=models.CharField(max_length=50)
     data_nascimento=models.DateField()
     descricao=models.TextField(blank=True,null=True)
     disponivel=models.BooleanField(default=True)
     raca=models.ForeignKey(Raca,on_delete=models.CASCADE,related_name='gatos')
+    foto=models.ImageField(upload_to='gatos/',blank=True,null=True)
 
     def __str__(self):
         return f'{self.nome} ({self.raca.nome})'
