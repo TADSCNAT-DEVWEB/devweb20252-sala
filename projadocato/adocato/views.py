a97118cd92a847ad4215f2d560f5b8550a598cb6
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-11-26 21:01:10 +0000

    Implementação da classe RacaService e das respectivas views e dos templates.

--- projadocato/adocato/views.py
+++ projadocato/adocato/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import Raca, Gato
 from django.core.exceptions import ValidationError
 from adocato.services.gatoservice import GatoService
+from adocato.services.racaservice import RacaService
 from adocato.utils import GerenciadorMensagem
 # Create your views here.
 
@@ -10,12 +11,12 @@ def index(request):
 
 def raca_list(request):
     if request.method=='GET':
-        racas=Raca.objects.all()
+        racas=RacaService.listar_racas()
     else:
         nome=request.POST.get('nome','')
-        racas=Raca.objects.filter(nome__icontains=nome)
+        racas=RacaService.buscar_racas(nome=nome)
     context={'racas':racas}
-    return render(request, 'adocato/racas.html',context)
+    return render(request, 'adocato/racas/lista.html',context)
 
 def gato_list_por_raca(request, raca_id):
     gatos=GatoService.listar_gatos_por_raca(raca_id)
@@ -23,7 +24,7 @@ def gato_list_por_raca(request, raca_id):
     return render(request, 'adocato/gatos/lista.html',context)
 
 def gato_cadastrar(request):
-    racas=Raca.objects.all()
+    racas=RacaService.listar_racas()
     if request.method=='POST':
         nome=request.POST.get('nome')
         sexo=request.POST.get('sexo')
@@ -42,7 +43,7 @@ def gato_cadastrar(request):
 
 def gato_editar(request, gato_id):
     gato=GatoService.obter_gato_por_id(gato_id)
-    racas=Raca.objects.all()
+    racas=RacaService.listar_racas()
     if not gato:
         return redirect('adocato:gato_list')
     if request.method=='POST':
@@ -88,3 +89,34 @@ def listar_gatos_disponiveis(request):
     gatos=GatoService.listar_gatos_disponiveis()
     context={'gatos':gatos}
     return render(request, 'adocato/gatos/lista.html',context)
+
+def raca_cadastrar(request):
+    if request.method=='POST':
+        nome=request.POST.get('nome')
+        try:
+            RacaService.cadastrar_raca(nome)
+            GerenciadorMensagem.processar_mensagem_sucesso(request, 'Raça cadastrada com sucesso!')
+            return redirect('adocato:raca_list')
+        except ValidationError as e:
+            GerenciadorMensagem.processar_mensagem_erro(request, e)
+    return render(request, 'adocato/racas/form.html')
+
+def raca_editar(request, raca_id):
+    raca=RacaService.obter_raca_por_id(raca_id)
+    if not raca:
+        return redirect('adocato:raca_list')
+    if request.method=='POST':
+        nome=request.POST.get('nome')
+        try:
+            RacaService.atualizar_raca(raca_id, nome)
+            GerenciadorMensagem.processar_mensagem_sucesso(request, 'Raça atualizada com sucesso!')
+            return redirect('adocato:raca_list')
+        except ValidationError as e:
+            GerenciadorMensagem.processar_mensagem_erro(request, e)
+    context={'raca':raca}
+    return render(request, 'adocato/racas/form.html',context)
+
+def raca_excluir(request, raca_id):
+    RacaService.excluir_raca(raca_id)
+    GerenciadorMensagem.processar_mensagem_sucesso(request, 'Raça excluída com sucesso!')
+    return redirect('adocato:raca_list')
